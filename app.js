79fb353b5ee1ade10e295fe8b3f6187c1e1f172a
martynas <martynas.mikalajunas@gmail.com>
2018-12-02 23:42:38 +0200

    added handler for root path

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const {get} = require ('./lib/apify');
 get('/hello', params => {
   return Promise.resolve([200, {result: 'hello, my dear friend!', params}]);
 });
+
+get('/', params => {
+  return Promise.resolve([200, {result: 'try /hello', params}]);
+});
