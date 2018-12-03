1008e8aaf95d46763e106cb2b7172a4753808fc4
martynas <martynas.mikalajunas@gmail.com>
2018-12-03 22:34:33 +0200

    fixed url matching. added colors to output

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const {get, post} = require ('./lib/apify');
 
 
-get('hello', params => {
+get('/hello', params => {
   return Promise.resolve([200, {result: 'hello, my dear friend!', params}]);
 });
 
-get('', params => {
+get('/', params => {
   return Promise.resolve([200, {result: 'try /hello', params}]);
 });
 
-post('api/data', params => {
+post('/api/data', params => {
   return Promise.resolve([200, {result: 'ok', params}]);
 });
